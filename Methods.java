b16b8e54c222c731c6f9b4d0f52e18b0719e899c
JasonHuang1213 <37361235+JasonHuang1213@users.noreply.github.com>
2020-07-11 21:38:02 +0300

    Delete Methods.java

--- Methods.java
+++
@@ -1,29 +0,0 @@
-public class Methods {
-	// declaration of method foo
-	private static void foo() {
-		System.out.println("Begin foo");
-		// call for method bar
-		bar();
-		System.out.println("Last sentence of method foo");
-	}
-	// declaration of method bar
-	private static void bar() {
-		System.out.println("Begin bar");
-		// call for method foobar
-		foobar();
-		System.out.println("Last sentence of method bar");
-	}
-	// declaration of method foobar
-	private static void foobar() {
-		System.out.println("Begin foobar");
-		System.out.println("Last sentence of method foobar");
-	}
-	// declaration of main method
-	public static void main(String[] args) {
-		System.out.println("call for foo() in main method");
-		foo();
-		System.out.println("---");
-		System.out.println("call for bar() in main method");
-		bar();
-	}
-}
